caef61dc917e545c9da1ef329fc1ccccbdaebefe
kronix1010 <alex20060420@gmail.com>
2025-07-27 18:16:28 -0500

    Added NavBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import Navbar from '$lib/Navbar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<Navbar />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,1 @@
-<h1 class="text-3xl font-bold text-purple-600 underline">
-  Hello, SvelteKit and Tailwind v4!
-</h1>
+<!-- Empty page -->
