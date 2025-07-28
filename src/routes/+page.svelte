9d8dd42f4b8e77f43a0c16322410a93975128b2c
kronix1010 <alex20060420@gmail.com>
2025-07-27 21:07:49 -0500

    Main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<!-- Empty page -->
+<script lang="ts">
+  import Folder from '$lib/components/Folder.svelte';
+</script>
+
+<div class="min-h-screen bg-[url('/images/kali-legacy.png')] bg-cover bg-center">
+  <div class="flex flex-col items-start gap-5 p-10">
+    <Folder label="Home" href="/" />
+    <Folder label="Projects" href="/projects" />
+    <Folder label="About" href="/about" />
+  </div>
+</div>
