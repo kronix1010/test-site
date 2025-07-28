73a06a7f04ddf2b47af70273cf268dded9b8f8fd
kronix1010 <alex20060420@gmail.com>
2025-07-27 22:04:21 -0500

    Add font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,7 @@
   import Folder from '$lib/components/Folder.svelte';
 </script>
 
-<div
-  class="min-h-screen bg-[url('/images/kali-legacy.png')] bg-cover bg-right md:bg-center"
->
+<div class="min-h-screen bg-[url('/images/kali-legacy.png')] bg-cover bg-center">
   <div class="flex flex-col items-start gap-4 p-4 md:gap-5 md:p-10">
     <Folder label="Home" href="/" />
     <Folder label="Projects" href="/projects" />
