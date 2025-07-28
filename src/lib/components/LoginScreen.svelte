ceee7492cd21179154d04d9d9b4ce5842f0852c8
kronix1010 <alex20060420@gmail.com>
2025-07-27 22:25:35 -0500

    Add 'login' screen with transition

---
+++ src/lib/components/LoginScreen.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { onMount, createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  let username = '';
+  let password = '';
+
+  const typewriter = (text: string, update: (val: string) => void, delay: number): Promise<void> => {
+    return new Promise((resolve) => {
+      let i = 0;
+      const interval = setInterval(() => {
+        if (i < text.length) {
+          update(text.slice(0, i + 1));
+          i++;
+        } else {
+          clearInterval(interval);
+          resolve();
+        }
+      }, delay);
+    });
+  };
+
+  onMount(async () => {
+    const runAnimation = async () => {
+      await typewriter('Alex Rodriguez', (val) => (username = val), 100);
+      await typewriter('**********', (val) => (password = val), 50);
+
+      setTimeout(() => {
+        dispatch('animationEnd');
+      }, 500);
+    };
+
+    runAnimation();
+  });
+</script>
+
+<div class="fixed inset-0 bg-[#28447c] flex items-center justify-center p-4">
+  <div class="bg-gray-800 rounded-lg p-8 max-w-sm w-full">
+    <div class="bg-gray-900 p-3 rounded h-10 font-mono mb-4 text-white">{username}</div>
+    <div class="bg-gray-900 p-3 rounded h-10 font-mono text-white">{password}</div>
+    <div class="flex justify-end gap-4 mt-6">
+      <div class="text-white">Cancel</div>
+      <div class="bg-blue-600 text-white px-4 py-2 rounded-lg">Log in</div>
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
   import Folder from '$lib/components/Folder.svelte';
+  import LoginScreen from '$lib/components/LoginScreen.svelte';
+  import { fade } from 'svelte/transition';
+
+  let showLogin = true;
+
+  function handleAnimationEnd() {
+    showLogin = false;
+  }
 </script>
 
-<div class="min-h-screen bg-[url('/images/kali-legacy.png')] bg-cover bg-center">
-  <div class="flex flex-col items-start gap-4 p-4 md:gap-5 md:p-10">
-    <Folder label="Home" href="/" />
-    <Folder label="Projects" href="/projects" />
-    <Folder label="About" href="/about" />
+{#if showLogin}
+  <LoginScreen on:animationEnd={handleAnimationEnd} />
+{:else}
+  <div
+    transition:fade={{ duration: 500, delay: 250 }}
+    class="min-h-screen bg-[url('/images/kali-legacy.png')] bg-cover bg-center"
+  >
+    <div class="flex flex-col items-start gap-4 p-4 md:gap-5 md:p-10">
+      <Folder label="Home" href="/" />
+      <Folder label="Projects" href="/projects" />
+      <Folder label="About" href="/about" />
+    </div>
   </div>
-</div>
+{/if}
